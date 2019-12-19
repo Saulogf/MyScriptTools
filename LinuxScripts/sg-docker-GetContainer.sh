c31c12924e78e557b16a6e5f6b8c21143c66e9e0
Saulo Freitas <saulogfreitas@gmail.com>
2019-12-19 22:00:36 +0000

    Add Linux file

---
+++ LinuxScripts/sg-docker-GetContainer.sh
@@ -0,0 +1,13 @@
+﻿param ($cn = $(throw "Nome do Container é necessário!"))
+
+function GetContainerIP {
+  $error.clear()
+  $container_ip = docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cn
+  $Ip_check = ($container_ip -AS [IPAddress]) -as [Bool]
+  if ($Ip_check -ne "TRUE")
+      {Write-Host "Erro!"}
+  else
+      {Write-Host "O IP do servidor $cn é: $container_ip"}
+}
+
+GetContainerIP($cn)
