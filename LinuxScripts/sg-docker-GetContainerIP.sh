3333984935e459d93a917ec23231bec880e9c774
Saulo Freitas <saulogfreitas@gmail.com>
2020-01-09 11:46:13 +0000

    just another change

--- LinuxScripts/sg-docker-GetContainerIP.sh
+++ LinuxScripts/sg-docker-GetContainerIP.sh
@@ -1,14 +1,15 @@
-﻿#!/usr/bin/bash
-param ($cn = $(throw "Nome do Container é necessário!"))
+﻿#!/bin/bash
+
+#param ($cn = $(throw "Nome do Container é necessário!"))
 
 function GetContainerIP {
-  $error.clear()
-  $container_ip = docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cn
-  $Ip_check = ($container_ip -AS [IPAddress]) -as [Bool]
+
+  container_ip = docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cn
+  Ip_check = ($container_ip -AS [IPAddress]) -as [Bool]
   if ($Ip_check -ne "TRUE")
-      {Write-Host "Erro!"}
+       {echo "Erro!"}
   else
-      {Write-Host "O IP do servidor $cn é: $container_ip"}
+      {echo "O IP do servidor $cn é: $container_ip"}
 }
 
 GetContainerIP($cn)
